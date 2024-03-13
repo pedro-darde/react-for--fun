a325a4250dfd6e5d369e1c31bf9808e228d249f4
pedro.darde <pedro.darde@multi24h.com.br>
2024-03-13 13:40:56 -0300

    auth components

--- src/main.tsx
+++ src/main.tsx
@@ -9,5 +9,5 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Toaster richColors />
     <RouterProvider router={router} />
-  </React.StrictMode>,
+  </React.StrictMode>
 );
--- src/router.tsx
+++ src/router.tsx
@@ -3,12 +3,17 @@ import Home from "@/components/home";
 import Root from "@/components/root";
 import ListTagsPage from "@/pages/tags/list/ListTags";
 import ListRecipe from "./pages/recipes/list/ListRecipe";
+console.log("browserrouter");
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
+
     children: [
-      { index: true, element: <Home /> },
+      {
+        index: true,
+        element: <Home />,
+      },
       { path: "tags", element: <ListTagsPage /> },
       { path: "recipes", element: <ListRecipe /> },
     ],
